4a6844ff064072b409e9c2f89b8ab62968b20fc5
norma13115 <normapadilla13115@gmail.com>
2024-08-22 03:54:05 +0000

    updated js script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// all code for this lab to be completed in this file
+let currentTemperature = 74
+
+//is the temperature freezing ?
+if(currentTemperature >= 32){
+    console.log("Summer is not over!!!!")
+} else{
+    console.log("If you hate summer you should of moved out of Imperial valley!!")
+}
+const myAge = 31
+const yourAge = 38
+if (myAge===yourAge){
+    console.log ("we are so young")
+} else if (myAge>yourAge){
+    console.log ("I am olderthan you.")
+} else {
+    console.log ("we are here")
+}
